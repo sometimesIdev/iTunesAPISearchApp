d520e1cde626aec907afecb28e27a33a92799418
Pat Butler <pcbutler@telus.net>
2022-09-10 09:55:03 -0700

    Initial Commit

---
+++ iTunesAPISearchApp/iTunesAPISearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesAPISearchAppApp.swift
+//  iTunesAPISearchApp
+//
+//  Created by Pat Butler on 2022-09-10.
+//
+
+import SwiftUI
+
+@main
+struct iTunesAPISearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
